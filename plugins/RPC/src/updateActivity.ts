47159a506aea5bd454f6a54b5ad8402f0af1c4ce
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-08-17 19:03:46 -0400

    Update updateActivity.ts

--- plugins/RPC/src/updateActivity.ts
+++ plugins/RPC/src/updateActivity.ts
@@ -13,7 +13,7 @@ const fmtStr = (s?: string, dc?: boolean) => {
 	if (s.length < 2) s += " ";
 
 	if (dc) {
-		s = s.replace(/\b\w/g, c => c.toUpperCase());
+		s = s.replace(/\b[a-z]/g, match => match.toUpperCase());
 		return s.length > DC_STR_MAX_LEN
 			? s.slice(0, DC_STR_MAX_LEN - 3) + "..."
 			: s;
